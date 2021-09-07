e5dd8540b72ae3c8487368bcfdd6290bce63eaec
Shannon Sorensen <sorensen.sms@gmail.com>
2021-09-07 15:42:56 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(recipients, event) {
+    let messages = [];
+    for(let i = 0; i < recipients.length; i++) {
+        messages.push(`Thank you, ${recipients[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+
+function countDown(posNum) {
+    while(posNum >= 0) {
+        console.log(posNum);
+        posNum--;
+
+    }
+    
+}
